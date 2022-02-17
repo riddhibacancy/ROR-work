731ff577c609d0259dc392d129656be25adbaa2d
Akash Sondarva <akash.sondarva@bacancy.com>
2022-02-17 17:08:59 +0530

    question ans done

---
+++ Assignment5/string.rb
@@ -0,0 +1,49 @@
+# difference between (" ") and (' ')
+
+    name = "Akash"
+    # puts "your name is #{name} "
+    # puts 'your name is #{name}'
+
+    # puts "Akash \n Sondarva"
+    # puts 'Akash \n sondarva'
+
+# Access String Elements
+
+    str = "hello Akash welcome back"
+
+    # puts str["hello"]
+    # puts str[1]
+    # puts str[0,11]
+
+# String Replication
+
+    str = "Akash\n"
+    # puts str * 7
+
+#size,length
+
+    str = "Akash\n"
+    # puts str.size
+
+
+# <<  ("Add sondarva behind Akash")
+    s = "Akash "
+    s << "Sondarva"
+    # puts s 
+
+# string comparison
+    # -1 if other_string is larger.
+    # 0 if the two are equal.
+    # 1 if other_string is smaller.
+
+        # puts 'akash' <=> 'akash' 
+        # puts 'akash' <=> 'akashhhh'    
+        # puts 'akashhh' <=> 'akash'
+
+
+# byteslice
+        s = '0123456789' 
+        s = "Akash Sondarva"
+    # puts  s.byteslice(2)   
+    # puts s.byteslice(4, 3) 
+    # puts s.byteslice(6, 30) 
